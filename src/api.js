0d613e7cdc8860b3c6f1e1ab5e35106359f54b58
Ullas Kiran <ullas.kiran@ilearningengines.com>
2022-08-10 22:04:44 +0530

    created weather app live updation

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOption = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3e7bc24832msh00a05cf604eeee2p1fb94ejsnbc573c42e596",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "fd684935121a37102944c810a6f27d67";
